b51517e2e1ac9cca738898d5fb9a3a9fb05ac392
Rafael <rafaelproinfo@gmail.com>
2022-05-10 00:55:21 -0300

    Update Security

--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
@@ -5,11 +5,12 @@ import java.util.List;
 import com.generation.blogpessoal.model.Tema;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TemaRepository extends JpaRepository<Tema, Long>{
     
-    public List <Tema> findAllByDescricaoContainingIgnoreCase (String descricao);
+    public List <Tema> findAllByDescricaoContainingIgnoreCase (@Param("descricao") String descricao);
 
 }
