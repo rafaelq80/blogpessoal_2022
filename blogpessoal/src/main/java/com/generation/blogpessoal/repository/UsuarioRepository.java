88c89585bd4ef1f5b9e4409b07f6e1e535e78bd7
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2022-05-13 04:44:44 -0300

    Update UsuarioRepository.java

--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
@@ -14,6 +14,6 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 	public Optional<Usuario> findByUsuario(String usuario);
 
-    public List<Usuario> findAllByNomeContainingIgnoreCase(@Param("usuario") String usuario);
+    public List<Usuario> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 
 }
