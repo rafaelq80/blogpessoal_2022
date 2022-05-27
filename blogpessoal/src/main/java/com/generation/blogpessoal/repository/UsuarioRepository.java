8b70d337f70cd399806980e6f801063264e3c833
Rafael <rafaelproinfo@gmail.com>
2022-05-27 03:22:44 -0300

    Update Swagger

--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import com.generation.blogpessoal.model.Usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,6 +14,6 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 	public Optional<Usuario> findByUsuario(String usuario);
 
-    public List<Usuario> findAllByNomeContainingIgnoreCase(String string);
+    public List<Usuario> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 
 }
