814c2831b2959087aad88631f0033680bf655953
Rafael <rafaelproinfo@gmail.com>
2022-12-02 18:10:22 -0300

    Update Security

--- blogpessoal/src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
@@ -1,95 +1,41 @@
 package com.generation.blogpessoal.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * Classe BasicSecurityConfig
  * 
  * Esta classe é responsável por habilitar a segurança básica da aplicação e o login
- * na aplicação.
- * 
- * Para habilitar a segurança HTTP no Spring, precisamos extender (herdar) 
- * a Classe WebSecurityConfigurerAdapter para fornecer uma configuração padrão 
- * no método configure (HttpSecurity http)
+ * na aplicação. Nesta Classe vamos habilitar a autenticação http.
  * 
  * A configuração padrão garante que qualquer requisição enviada para a API 
- * seja autenticada com login baseado em formulário ou autenticação via Browser.
- * 
- * Para personalizar a autenticação utilizaremos a sobrecarga dos métodos da
- * Classe WebSecurityConfigurerAdapter
+ * seja autenticada com login baseado em formulário.
  * 
  */
 
 /**
+ *  A annotation @Configuration: indica que a Classe é do tipo configuração, ou seja, 
+ *  define uma Classe como fonte de definições de Beans, além de ser uma das anotações 
+ *  essenciais ao utilizar uma configuração baseada em Java.
+ * 
  *  A annotation @EnableWebSecurity: habilita a configuração de segurança padrão 
  *  do Spring Security na nossa api.
  */
-@SuppressWarnings("deprecation")
+@Configuration
 @EnableWebSecurity
-public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
-
-	/**
-	 * A annotation @Autowired insere uma Injeção de Dependência. 
-	 * 
-	 * Como iremos utilizar os usuários salvos no nosso Banco de dados,
-	 * na tabela tb_usuarios, para efetuar o login na api precisamos injetar 
-	 * um objeto da Interface UserDetailsService que será implementada na 
-	 * Classe UserDetailsServiceImpl que fará o acesso ao nosso Banco de dados
-	 * para recuperar os dados do usuário.
-	 */
-
-	@Autowired
-	private UserDetailsService userDetailsService;
-
-	/**
-	 *  Sobrescreve (@Override) o primeiro método Configure, que tem a função 
-	 *  de criar uma nova instância da Classe AuthenticationManagerBuilder e 
-	 *  define que o login será efetuado através dos usuários criados no Banco de dados.
-	 *  Para recuperar os dados do usuário no Banco de Dados utilizaremos a Interface 
-	 *  UserDetailsService.
-	 *  Outra alternativa de login seria acriação de um usuário em memória, que veremos nas
-	 *  próximas sessões.
-	 *  
-	 *  O método é do tipo protected por definição da classe.
-	 * 
-	 *  Lembrete:
-	 * 
-	 *  1) public: permite acesso a qualquer código externo a classe.
-	 *  2) protected: permite acesso às classes filhas, mas proíbe a qualquer 
-	 *     outro acesso externo.
-	 *  3) private: proíbe qualquer acesso externo à própria classe, inclusive 
-	 *     das classes filhas.
-	 */
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
-		/**
-		 *  O objeto auth registra e cria uma nova instância do objeto userDetailsService
-		 *  da interface UserDetailsService implementada na Classe UserDetailsServiceImpl
-		 *  para recuperar os dados dos usuários gravados no Banco de dados.
-		 */
-		
-		 auth.userDetailsService(userDetailsService);
-
-		 auth.inMemoryAuthentication()
-			.withUser("root")
-			.password(passwordEncoder().encode("root"))
-			.authorities("ROLE_USER");
-
-	}
-
-	/**
+public class BasicSecurityConfig {
+	
+    /**
 	 *  A annotation @Bean transforma a instância retornada pelo método como um 
 	 *  objeto gerenciado pelo Spring, desta forma, ele pode ser injetado em qualquer
 	 *  classe, a qualquer momento que você precisar sem a necessidade de usar a 
@@ -105,13 +51,34 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	/**
-	 *  Sobrescreve (@Override) o segundo método Configure que é responsável por
-	 *  criar uma instância da Classe HttpSecurity, que permite configurar a 
-	 *  segurança baseada na web para solicitações http específicas (endpoints)
-	 */
-	
-	 @Override
-	protected void configure(HttpSecurity http) throws Exception {
+     * O Método authenticationManager(AuthenticationConfiguration authenticationConfiguration), 
+     * implementa a configuração de autenticação. 
+     * 
+     * Este Método utiliza o Método authenticationConfiguration.getAuthenticationManager() para 
+     * procurar uma implementação da Interface UserDetailsService e utilizá-la para identificar 
+     * se o usuário é válido ou não. 
+     * 
+     * Em nosso projeto Blog Pessoal, será utilizada a Classe UserDetailsServiceImpl, que valida 
+     * o usuário no Banco de dados.
+     * 
+     */
+	@Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+	/** O Método SecurityFilterChain filterChain(HttpSecurity http), informao ao
+	 *  Spring que a configuração padrão da Spring Security será substituída 
+	 *  por uma nova configuração. 
+	 * 
+	 * Nesta configuração iremos customizar a autenticação da aplicação desabilitando
+	 * o formulário de login e habilitando a autenticação via http. 
+	 * 
+	 * */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
 		
 		/**
 		 * antMatchers().permitAll -> Endpoint liberado de autenticação
@@ -124,8 +91,6 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 		 * 
 		 * anyRequest().authenticated() -> Todos os demais endpoints 
 		 * serão autenticados
-		 * 
-		 * httpBasic -> Tipo de autenticação http (Basic Security)
 		 * 
 		 * Http Sessions: As sessões HTTP são um recurso que permite 
 		 * que os servidores da Web mantenham a identidade do usuário 
@@ -159,15 +124,21 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 		 * 
 		 */
 
-		http.authorizeRequests()
-			.antMatchers("/usuarios/logar").permitAll()
-			.antMatchers("/usuarios/cadastrar").permitAll()
-			.antMatchers(HttpMethod.OPTIONS).permitAll()
-			.anyRequest().authenticated()
-			.and().httpBasic()
-			.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and().cors()
-			.and().csrf().disable();
+		http
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().csrf().disable()
+            .cors();
+
+        http
+            .authorizeHttpRequests((auth) -> auth
+                .antMatchers("/usuarios/logar").permitAll()
+                .antMatchers("/usuarios/cadastrar").permitAll()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .anyRequest().authenticated())
+            .httpBasic();
+
+        return http.build();
 			
 	}
 }
